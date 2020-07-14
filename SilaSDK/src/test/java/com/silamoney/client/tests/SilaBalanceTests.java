b0cac61c0756727db318d2bbbe2a8eb1334ae285
Sydney McMuldroch <sydney@ip-10-0-0-252.us-west-2.compute.internal>
2020-07-14 13:30:25 -0700

    test doesn't need to be commented

--- SilaSDK/src/test/java/com/silamoney/client/tests/SilaBalanceTests.java
+++ SilaSDK/src/test/java/com/silamoney/client/tests/SilaBalanceTests.java
@@ -15,11 +15,11 @@ public class SilaBalanceTests {
 	SilaApi api = new SilaApi(DefaultConfigurations.host, DefaultConfigurations.appHandle,
 			DefaultConfigurations.privateKey);
 
-//	@Test
-//	public void Response200Success() throws Exception {
-//		ApiResponse response = api.silaBalance(DefaultConfigurations.host, "0xabc123abc123abc123");
-//
-//		assertEquals(200, response.getStatusCode());
-//		assertEquals("1000", response.getData());
-//	}
+	@Test
+	public void Response200Success() throws Exception {
+		ApiResponse response = api.silaBalance(DefaultConfigurations.host, "0xabc123abc123abc123");
+
+		assertEquals(200, response.getStatusCode());
+		assertEquals("1000", response.getData());
+	}
 }
