ee96c60871c9d3a3b78b09c45938508be59ad7b0
Karlo <klorenzana@digitalgeko.com>
2021-02-10 14:04:12 -0600

    Adding limits to entity name in register business test

--- SilaSDK/src/test/java/com/silamoney/client/tests/RegisterBusinessTests.java
+++ SilaSDK/src/test/java/com/silamoney/client/tests/RegisterBusinessTests.java
@@ -23,7 +23,37 @@ public class RegisterBusinessTests {
 			DefaultConfigurations.privateKey);
 
 	@Test
-	public void Response200() throws Exception {
+	public void Response_001_400_2_Chars() throws Exception {
+		BusinessType businessType = DefaultConfigurations.getBusinessTypes().get(0);
+		NaicsCategoryDescription naicsCategory = DefaultConfigurations.getDefaultNaicCategoryDescription();
+		BusinessUser user = new BusinessUser(DefaultConfigurations.getBusinessHandle(), "Office", "123 Main Street",
+				null, "New City", "OR", "97204-1234", "503-123-4567", "example@silamoney.com", "123452222",
+				DefaultConfigurations.getBusinessCryptoAddress(), "en", businessType,
+				"https://www.website.com", "doing business as", naicsCategory, true);
+
+		ApiResponse response = api.registerBusiness(user, new Device("1234567890"));
+
+		assertEquals(400, response.getStatusCode());
+	}
+
+	@Test
+	public void Response_002_400_200_Chars() throws Exception {
+		BusinessType businessType = DefaultConfigurations.getBusinessTypes().get(0);
+		NaicsCategoryDescription naicsCategory = DefaultConfigurations.getDefaultNaicCategoryDescription();
+		BusinessUser user = new BusinessUser(DefaultConfigurations.getBusinessHandle(), "Office", "123 Main Street",
+				null, "New City", "OR", "97204-1234", "503-123-4567", "example@silamoney.com", "123452222",
+				DefaultConfigurations.getBusinessCryptoAddress(), 
+				"L1mcysCdEHXWJdeQnj7xMJIhjYMPWq2qS7sa7U8D2GfZyR0ID5gYrYmEF8qZgInNFa1zSsD2dyKbPlzGgiMeMWpnOc1SHxdyjDFL84R8MMUApQ1WRiUoIGIqyaF3BoPVTOn8v14al51P4g6JckAxZFtLI9Udte5S6JGiYVBuFe2MUad8SyjSHB6QIrNziV7vsxewkE1n2"
+				, businessType,
+				"https://www.website.com", "doing business as", naicsCategory, true);
+
+		ApiResponse response = api.registerBusiness(user, new Device("1234567890"));
+
+		assertEquals(400, response.getStatusCode());
+	}
+
+	@Test
+	public void Response_003_200() throws Exception {
 		BusinessType businessType = DefaultConfigurations.getBusinessTypes().get(0);
 		NaicsCategoryDescription naicsCategory = DefaultConfigurations.getDefaultNaicCategoryDescription();
 		BusinessUser user = new BusinessUser(DefaultConfigurations.getBusinessHandle(), "Office", "123 Main Street",
