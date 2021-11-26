7e6dbd73172d2ffb021bea7dc8d652bc82cd6e3a
aditi <aditi@arcgate.com>
2021-11-26 12:48:22 +0530

    Fixing qualification_details

--- SilaSDK/src/main/java/com/silamoney/client/domain/CheckInstantAchResponse.java
+++ SilaSDK/src/main/java/com/silamoney/client/domain/CheckInstantAchResponse.java
@@ -4,9 +4,15 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 import lombok.Getter;
 
+import java.util.Map;
+
 @Getter
 public class CheckInstantAchResponse extends BaseResponse {
     @Expose
     @SerializedName("maximum_amount")
     private String maximum_amount;
+
+    @Expose
+    @SerializedName("qualification_details")
+    private Map<String, Object> qualificationDetails;
 }
