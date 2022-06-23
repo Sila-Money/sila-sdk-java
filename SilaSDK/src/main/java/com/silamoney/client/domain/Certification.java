4c83921d184e865f516d91b1b663c0e78926d587
Alok Singh <alok.singh@iconsavingsplan.com>
2022-06-23 15:31:39 -0700

    Local changes to match API response

---
+++ SilaSDK/src/main/java/com/silamoney/client/domain/Certification.java
@@ -0,0 +1,25 @@
+package com.silamoney.client.domain;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Builder
+@Data
+public class Certification {
+    
+    @SerializedName("administrator_user_handle")
+    private String administratorUserHandle;
+    private String created;
+    @SerializedName("created_epoch")
+    private Long createdEpoch;
+    @SerializedName("expires_after")
+    private String expiresAfter; 
+    @SerializedName("expires_after_epoch")
+    private Long expiresAfterEpoch;
+    @SerializedName("beneficial_owner_certifications")
+    private List<String> beneficialOwnerCertifications;  
+
+}
