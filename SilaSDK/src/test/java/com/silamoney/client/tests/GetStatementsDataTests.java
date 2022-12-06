9e01c3d620c71f24e0a2fab359f2934be98aa1a8
jared <jared@creating.agency>
2022-12-05 21:03:50 -0600

    ENGRG-617: Added test coverage showing that all features of GetStatementsData are covered in tests.

--- SilaSDK/src/test/java/com/silamoney/client/tests/GetStatementsDataTests.java
+++ SilaSDK/src/test/java/com/silamoney/client/tests/GetStatementsDataTests.java
@@ -2,13 +2,24 @@ package com.silamoney.client.tests;
 
 import com.silamoney.client.api.ApiResponse;
 import com.silamoney.client.api.SilaApi;
-import com.silamoney.client.domain.GetStatementsResponse;
-import com.silamoney.client.domain.StatementSearchFilters;
+import com.silamoney.client.domain.*;
 import com.silamoney.client.testsutils.DefaultConfigurations;
+import org.joda.time.LocalDate;
 import org.junit.Test;
+import org.web3j.crypto.*;
+import org.web3j.crypto.Wallet;
+
+import java.math.BigInteger;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class GetStatementsDataTests {
 
@@ -18,15 +29,120 @@ public class GetStatementsDataTests {
     @Test
     public void Response200() throws Exception {
         StatementSearchFilters searchFilters = new StatementSearchFilters();
+        int expectedStatementSize = 10;
         searchFilters.setPage(1);
-        searchFilters.setPerPage(20);
-        searchFilters.setMonth("11-2022");
+        searchFilters.setPerPage(expectedStatementSize);
+        searchFilters.setMonth("10-2022");
 
         ApiResponse response = api.getStatementsData(DefaultConfigurations.getUserHandle(), DefaultConfigurations.getUserPrivateKey(), searchFilters);
         assertEquals(200, response.getStatusCode());
         GetStatementsResponse parsedResponse = (GetStatementsResponse) response.getData();
-        assertTrue(parsedResponse.getStatements().size() > 0);
+
         assertEquals("SUCCESS", parsedResponse.getStatus());
+        assertEquals(true, parsedResponse.getSuccess());
+
+        // Get the most important objects in the response for testing
+        Pagination pages = parsedResponse.getPagination();
+        List<Statement> statements = parsedResponse.getStatements();
+        Statement statement = statements.get(0);
+        TransactionEntity transaction = statement.getTransactions().get(0);
+
+        // Test pagination and response size
+        assertNotNull(pages);
+        assertNotEquals(pages.getReturnedCount(), 0);
+        assertNotNull(parsedResponse.getReference());
+        assertNotNull(parsedResponse.getResponseTimeMs());
+        assertNotNull(parsedResponse.getStatements());
+        assertEquals(pages.getReturnedCount(), expectedStatementSize);
+        assertEquals(pages.getReturnedCount(), statements.size());
+        assertEquals(pages.getTotalCount(), parsedResponse.getTotalCount());
+        assertEquals(pages.getReturnedCount(), parsedResponse.getReturnedCount());
+
+        // Test statement fields
+        assertNotNull(statement);
+        assertNotNull(statement.getUserHandle());
+        assertNotNull(statement.getDate());
+        assertNotNull(statement.getFirstName());
+        assertNotNull(statement.getLastName());
+        assertNotNull(statement.getWalletId());
+        assertNotNull(statement.getBeginningBalance());
+        assertNotNull(statement.getEndingBalance());
+
+        // Test transaction fields
+        assertNotNull(transaction); assertNotNull(transaction.getSettledDate()); assertNotNull(transaction.getDescription());
+        assertNotNull(transaction.getCategory());
+        assertNotNull(transaction.getAmount());
+        assertNotNull(transaction.getCategory());
+        assertNotNull(transaction.getRunningBalance());
+
+    }
+
+    @Test
+    public void Response400WithoutPageFilter() throws Exception {
+        StatementSearchFilters searchFilters = new StatementSearchFilters();
+        searchFilters.setPerPage(20);
+        searchFilters.setMonth("11-2022");
+        ApiResponse response = api.getStatementsData(DefaultConfigurations.getUserHandle(),
+                DefaultConfigurations.getUserPrivateKey(), searchFilters);
+        assertEquals(400, response.getStatusCode());
+    }
+
+    @Test
+    public void Response400WithoutPerPageFilter() throws Exception {
+        StatementSearchFilters searchFilters = new StatementSearchFilters();
+        searchFilters.setPage(1);
+        searchFilters.setMonth("11-2022");
+        ApiResponse response = api.getStatementsData(DefaultConfigurations.getUserHandle(),
+                DefaultConfigurations.getUserPrivateKey(), searchFilters);
+        assertEquals(400, response.getStatusCode());
+    }
+
+    @Test
+    public void Response400WithoutMonthFilter() throws Exception {
+        StatementSearchFilters searchFilters = new StatementSearchFilters();
+        searchFilters.setPage(1);
+        searchFilters.setPerPage(20);
+        ApiResponse response = api.getStatementsData(DefaultConfigurations.getUserHandle(),
+                DefaultConfigurations.getUserPrivateKey(), searchFilters);
+        assertEquals(400, response.getStatusCode());
+    }
+
+    @Test
+    public void NewUser200Response() throws Exception {
+        StatementSearchFilters searchFilters = new StatementSearchFilters();
+        searchFilters.setPage(1);
+        searchFilters.setPerPage(20);
+        searchFilters.setMonth("11-2022");
+
+        String newUserHandle = "javaSDK-" + new Random().nextInt();
+        LocalDate birthdate = new LocalDate(2001, 6, 22);
+        String userPrivateKey = "";
+
+        try {
+            ECKeyPair ecKeyPair = Keys.createEcKeyPair();
+            BigInteger privateKeyInDec = ecKeyPair.getPrivateKey();
+            userPrivateKey = privateKeyInDec.toString(16);
+            Wallet.createLight(UUID.randomUUID().toString(), ecKeyPair);
+        } catch (InvalidAlgorithmParameterException | NoSuchAlgorithmException |
+                 NoSuchProviderException | CipherException e) {
+            e.printStackTrace();
+        }
+
+        new User(newUserHandle, "Example", "User", "123 Main Street",
+                null, "New City", "OR", "97204-1234", "503-123-4567",
+                newUserHandle + "@silamoney.com", "975865809",
+                userPrivateKey,
+                birthdate.toDate());
+
+        ApiResponse response = api.getStatementsData(newUserHandle, userPrivateKey, searchFilters);
+        assertEquals(200, response.getStatusCode());
+    }
+
+    @Test
+    public void UnregisteredUserResponse401() throws Exception {
+        ApiResponse response = api.getVirtualAccounts(DefaultConfigurations.getBusinessHandle(),
+                DefaultConfigurations.getUserPrivateKey());
+        assertEquals(401, response.getStatusCode());
     }
 
 }
