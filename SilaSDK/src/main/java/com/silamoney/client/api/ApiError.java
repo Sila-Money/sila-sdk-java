571a460cc9f495d0b365f80986615b06b940a4c7
Alok Singh <alok.singh@iconsavingsplan.com>
2023-04-19 09:46:34 -0700

    Capture and return error codes

--- SilaSDK/src/main/java/com/silamoney/client/api/ApiError.java
+++ SilaSDK/src/main/java/com/silamoney/client/api/ApiError.java
@@ -1,2 +1,28 @@
-package com.silamoney.client.api;public class ApiError {
+package com.silamoney.client.api;
+
+public class ApiError extends RuntimeException {
+    private final int code;
+    private final String errorBody;
+
+    public ApiError(int code, String message, String errorBody) {
+        super(message);
+        this.code = code;
+        this.errorBody = errorBody;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getErrorBody() {
+        return errorBody;
+    }
+
+    @Override
+    public String toString() {
+        return "ApiError{" +
+                "code=" + code +
+                ", errorBody='" + errorBody + '\'' +
+                '}';
+    }
 }
